73ab071c5b33e9aa1343e22f964bc44b713d48a7
akdavila2 <anakarina.daviladavila@gmail.com>
2022-01-20 17:57:53 -0600

    Agregando maquetacion de Home footer y header

---
+++ citi-hackathon/src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import accesibilidad from "../../assets/accesibilidadBlanco.png";
+import voz from "../../assets/Voz-azul.png";
+import tamañoLetra from "../../assets/TamañoDeFuente-predeterminado.png";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <img className="icon-footer" src={voz} alt="icon de voz" />
+        <img className="icon-footer" src={tamañoLetra} alt="icon de letra" />
+        <img className="icon-footer" src={accesibilidad} alt="icon accesibilidad" />
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
