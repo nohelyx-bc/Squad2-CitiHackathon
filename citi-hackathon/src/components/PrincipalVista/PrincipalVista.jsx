25ec0a67e62c322f7ee26f062c34711a8da57d80
akdavila2 <anakarina.daviladavila@gmail.com>
2022-01-21 14:20:52 -0600

    estilos y eventos de navegacion

--- citi-hackathon/src/components/PrincipalVista/PrincipalVista.jsx
+++ citi-hackathon/src/components/PrincipalVista/PrincipalVista.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const PrincipalVista = () => {
+  const navigate= useNavigate()
+  const handleTransferencias = () => {
+    navigate("/Transferencias");
+  };
+  const handleCuentasTerceros = () => {
+    navigate("/AltaBeneficiario");
+  };
   return (
     <main>
       <h3 className="titulos-predeterminado">BIENVENID@ USUARI@</h3>
-      <section className="botones">
-        <button className="boton-confirmar">TRANSFERENCIA</button>
-        <button className="boton-confirmar">AGREGAR CUENTAS A TERCEROS</button>
+      <section className="container-botones-principal">
+        <button className="boton-confirmar-principal" onClick={handleTransferencias}>TRANSFERENCIA</button>
+        <button className="boton-confirmar-principal" onClick={handleCuentasTerceros}>AGREGAR CUENTAS DE TERCEROS</button>
       </section>
-      <section className="info-container">
+      <section className="info-container-principal ">
         <p>CUENTA AHORRO CITIBANAMEX</p>
         <section>
           <p>***587</p>
