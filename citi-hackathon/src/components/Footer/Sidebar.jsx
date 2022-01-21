84d7a49c7c03a7ce83cf912cb607d30de8726c75
nasabisan <nasabisan@gmail.com>
2022-01-21 14:23:27 -0600

    Componente de sidebar

--- citi-hackathon/src/components/Footer/Sidebar.jsx
+++ citi-hackathon/src/components/Footer/Sidebar.jsx
@@ -1,21 +1,25 @@
 /* eslint-disable no-unused-vars */
 import React, {useState} from "react";
 import logoazul from "../../assets/CitiLogo-azul.png";
+import tamañoLetra from "../../assets/TamañoLetra-azul.png";
 
 const Sidebar = () => {
   const [open, setOpen] = useState(false);
   return (
     <div id="sidebar">
-      <div className="toggle-btn">
-        <span>&#9776</span>
-      </div>
       <ul>
-        <li>
+        <li className="center-item">
           <img src={logoazul} alt="Logo Citibanamex" className="logo"></img>
         </li>
-        <li>CONFIGURACION DE ACCESIBILIDAD</li>
-        <li>MODO OSCURO</li>
-        <li>ESCALA DE GRISES</li>
+        <li className="btn-title">CONFIGURACION DE ACCESIBILIDAD</li>
+        <li className="alinear-horizontal">
+        <img className="icon-pequeño" src={tamañoLetra} alt="Letra pequeña"/>
+        <img className="icon-mediano" src={tamañoLetra} alt="Letra mediana"/>
+        <img className="icon-grande" src={tamañoLetra} alt="Letra grande"/>
+      </li>
+        <li className="btn-oscuro">MODO OSCURO</li>
+        <li className="btn-grises">ESCALA DE GRISES</li>
+        <li className="btn-predefinida">PREDEFINIDA</li>
       </ul>
     </div>
   );
