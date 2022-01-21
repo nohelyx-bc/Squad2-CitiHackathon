381dacd480c5c8bc14808b2af9a7ae56344b4642
AngelicaJH <isc.florjardinez@gmail.com>
2022-01-21 14:42:26 -0600

    cambios de apimock

--- citi-hackathon/src/components/Home/Home.jsx
+++ citi-hackathon/src/components/Home/Home.jsx
@@ -2,16 +2,29 @@ import React from "react";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
-const Home = () => {
+const Home = ({dataUser}) => {
+  console.log('estoy en home', dataUser);
   // const styleBody={
   //   background:"red"
   // }
+  
   return (
     <>
       <div className="container-predeterminado" >
         <Header />
         <div className="render">
           <h1>Hola</h1>
+          {dataUser.tarjetas.map((val) => {
+          return (
+            <section key={val.id} className="content-items">
+              <div className="img-qty">
+                <img className="img-order" src={val.img_tarjeta} alt="coasa" />
+                {/* <p>{val.qty}</p> */}
+              </div>
+              {/* <p id="food-name">{val.name}</p> */}
+            </section>
+          );
+        })}
         </div>
         <Footer />
       </div>
--- citi-hackathon/src/routes/App.js
+++ citi-hackathon/src/routes/App.js
@@ -1,17 +1,44 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
+import { helpHttp } from "../helpers/helpHttp";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from '../components/Home/Home';
+// import Home from '../components/Home/Home';
 import Transferencias from '../components/Transferencias';
 import AltaBeneficiario from '../components/AltaBeneficiario';
 import ConfirmacionTransferencia from '../components/ConfirmacionTransferencia';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
+import { DataIteration } from "../components/Home/DataIteration";
 
-const app = () => {
+const App  = () => {
+  const[data, setData] = useState({});
+
+  // let url = `${process.env.REACT_APP_JSON_SERVER_USER}`;
+  let url='https://citibanamex-hackathon.herokuapp.com/User'
+  let api = helpHttp();
+
+  useEffect(() => {
+    // setLoading(true);
+    api.get(url).then((res) => {
+      if (!res.err) {
+        setData(res);
+        // setError(null);
+        
+       
+      } else {
+        setData(null);
+        
+        // setError(res);
+      }
+      // setLoading(false);
+    });
+  }, [url]);
+  console.log(data)
+
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+      {data && <Route exact path="/" element={<DataIteration dataUser={data}/>} />}
         <Route exact path="/Transferencias" element={<Transferencias />} />
         <Route exact path="/AltaBeneficiario" element={<AltaBeneficiario />} />
         <Route exact path="/ConfirmacionTransferencia" element={<ConfirmacionTransferencia />} />
@@ -22,4 +49,4 @@ const app = () => {
   );
 };
 
-export default app;
+export default App;
